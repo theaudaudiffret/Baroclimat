05c1c3cb2280a6899a1c9200d7d2682f09801eb7
Théau d'Audiffret <theaudaudiffret@MacBook-Air-de-Theau.local>
2025-04-04 15:58:43 +0200

    sub cat streamlit

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import my_pages.dashboard_2024 as dashboard
 import my_pages.methode as methode
 import my_pages.comparaison_modeles as comparaison
+import my_pages.Sous_categorie as sous_categorie
 
 st.set_page_config(page_title="Baroclima", layout="wide", initial_sidebar_state="collapsed")
 
@@ -10,7 +11,7 @@ st.set_page_config(page_title="Baroclima", layout="wide", initial_sidebar_state=
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox(
     "Choisissez une page :",
-    ["Dashboard 2024", "Méthode", "Comparaison des modèles"]
+    ["Dashboard 2024", "Méthode", "Comparaison des modèles", "Sous-catégorie"]
 )
 
 # Chargement dynamique des pages
@@ -22,3 +23,6 @@ elif page == "Méthode":
 
 elif page == "Comparaison des modèles":
     comparaison.show()
+
+elif page == "Sous-catégorie":
+    sous_categorie.show()
